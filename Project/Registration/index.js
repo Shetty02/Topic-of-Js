f4c958fef5801609f4a1c9bad6d810c445c70763
Shetty02 <shettynehal87@gmail.com>
2022-08-06 19:00:02 +0530

    L-5 Topic Covered

--- Project/Registration/index.js
+++ Project/Registration/index.js
@@ -7,7 +7,7 @@ function validate() {
     let zipInput = document.getElementById('zip').value
     let tnCInput = document.getElementById('tnC').checked
 
-    if(firstNameInput.length >= 1) {
+    if(firstNameInput) {
         document.getElementById('first-name-vaild').style.display = 'block'
         document.getElementById('first-name-invaild').style.display = 'none'
     }
@@ -15,7 +15,7 @@ function validate() {
         document.getElementById('first-name-invaild').style.display = 'block'
         document.getElementById('first-name-vaild').style.display = 'none'
     }
-    if(lastNameInput.length >= 1) {
+    if(lastNameInput) {
         document.getElementById('last-name-vaild').style.display = 'block'
         document.getElementById('last-name-invaild').style.display = 'none'
     }
@@ -29,7 +29,7 @@ function validate() {
     1)Wheter Exist--Done
     2) @
     3) .
-    4)After . last there should be atleaast 2 char
+    4)After last dot(.),there should be atleast 2 char
     5) First char cann't be @ */
     if(EmailInput && EmailInput.includes('@') && EmailInput.includes('.') && EmailInput.lastIndexOf('.') <= EmailInput.length - 3 && EmailInput.indexOf('@')!==0) {
         document.getElementById('Email-vaild').style.display = 'block'
@@ -39,7 +39,7 @@ function validate() {
         document.getElementById('Email-invaild').style.display = 'block'
         document.getElementById('Email-vaild').style.display = 'none'
     }
-    if(cityInput.length >= 1) {
+    if(cityInput) {
         document.getElementById('city-vaild').style.display = 'block'
         document.getElementById('city-invaild').style.display = 'none'
     }
@@ -47,7 +47,7 @@ function validate() {
         document.getElementById('city-invaild').style.display = 'block'
         document.getElementById('city-vaild').style.display = 'none'
     }
-    if(zipInput.length >= 1) {
+    if(zipInput) {
         document.getElementById('zip-vaild').style.display = 'block'
         document.getElementById('zip-invaild').style.display = 'none'
     }
