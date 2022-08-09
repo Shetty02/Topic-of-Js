c735ef4ee78e529adf4a8767afad0d8d895997cc
Shetty02 <shettynehal87@gmail.com>
2022-08-09 12:58:08 +0530

    Project Authnetication UI part Compelete

--- Project/Registration/index.js
+++ Project/Registration/index.js
@@ -7,21 +7,26 @@ function validate() {
     let zipInput = document.getElementById('zip').value
     let tnCInput = document.getElementById('tnC').checked
 
-    if(firstNameInput) {
+    let error = false;
+    
+    if(firstNameInput && !parseInt(firstNameInput)) {
         document.getElementById('first-name-vaild').style.display = 'block'
         document.getElementById('first-name-invaild').style.display = 'none'
     }
     else {
         document.getElementById('first-name-invaild').style.display = 'block'
         document.getElementById('first-name-vaild').style.display = 'none'
+        error = true;
     }
-    if(lastNameInput) {
+    
+    if(lastNameInput && !parseInt(lastNameInput)) {
         document.getElementById('last-name-vaild').style.display = 'block'
         document.getElementById('last-name-invaild').style.display = 'none'
     }
     else {
         document.getElementById('last-name-invaild').style.display = 'block'
         document.getElementById('last-name-vaild').style.display = 'none'
+        error = true;
     }
 
     /*
@@ -31,29 +36,72 @@ function validate() {
     3) .
     4)After last dot(.),there should be atleast 2 char
     5) First char cann't be @ */
-    if(EmailInput && EmailInput.includes('@') && EmailInput.includes('.') && EmailInput.lastIndexOf('.') <= EmailInput.length - 3 && EmailInput.indexOf('@')!==0) {
+    if (EmailInput && EmailInput.includes('@') && EmailInput.includes('.') && EmailInput.lastIndexOf('.') <= EmailInput.length - 3 && EmailInput.indexOf('@') !== 0) {
         document.getElementById('Email-vaild').style.display = 'block'
         document.getElementById('Email-invaild').style.display = 'none'
     }
     else {
         document.getElementById('Email-invaild').style.display = 'block'
         document.getElementById('Email-vaild').style.display = 'none'
+        error = true;
     }
-    if(cityInput) {
+    if(cityInput && cityInput.length >= 3) {
         document.getElementById('city-vaild').style.display = 'block'
         document.getElementById('city-invaild').style.display = 'none'
     }
     else {
         document.getElementById('city-invaild').style.display = 'block'
         document.getElementById('city-vaild').style.display = 'none'
+        error = true;
     }
-    if(zipInput) {
+
+    // console.log(parseInt(zipInput));
+    // console.log(!isNaN(parseInt(zipInput)));
+    if(zipInput && parseInt(zipInput) && zipInput.length === 6 ) {
         document.getElementById('zip-vaild').style.display = 'block'
         document.getElementById('zip-invaild').style.display = 'none'
     }
     else {
         document.getElementById('zip-invaild').style.display = 'block'
         document.getElementById('zip-vaild').style.display = 'none'
+        error = true;
     }
-console.log(firstNameInput, lastNameInput, EmailInput, cityInput, StateInput, zipInput, tnCInput);
+    if(StateInput != 'None'){
+        document.getElementById('state-valid').style.display='block';
+        document.getElementById('state-invalid').style.display='none';
+    }
+    else{
+        document.getElementById('state-invalid').style.display='block';
+        document.getElementById('state-valid').style.display='none';
+        error = true;
+    }
+
+    if(tnCInput) {
+        document.getElementById("tnC-invalid").style.display='none';
+    }
+    else {
+        document.getElementById("tnC-invalid").style.display='block';
+        error = true;
+    }
+    if(!error) {
+        alert('Your details have been saved successfully');
+        document.getElementById('first-name').value = '';
+        document.getElementById('last-name').value ='';
+        document.getElementById('Email').value= '';
+        document.getElementById('city').value ='';
+        document.getElementById('State').value = 'None';
+        document.getElementById('zip').value ='';
+        document.getElementById('tnC').checked = false;
+        document.getElementById('first-name-vaild').style.display = 'none'
+        document.getElementById('last-name-vaild').style.display = 'none'
+        document.getElementById('Email-vaild').style.display = 'none'
+        document.getElementById('city-vaild').style.display = 'none'
+        document.getElementById('zip-vaild').style.display = 'none'
+        document.getElementById('state-valid').style.display='none';
+        document.getElementById("tnC-invalid").style.display='none';
+
+
+         
+    }
+// console.log(firstNameInput, lastNameInput, EmailInput, cityInput, StateInput, zipInput, tnCInput);
 }
