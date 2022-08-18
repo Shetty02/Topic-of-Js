23ebbe3dacd123033f26a8558d259589dee98582
Shetty02 <shettynehal87@gmail.com>
2022-08-18 09:45:27 +0530

    16-Aug Lecture complete

---
+++ Project/OrderManagement/index.js
@@ -0,0 +1,19 @@
+// console.log("Nehal")
+
+
+const  executeOrder= () => {
+console.log('Order placed')
+let orderId = document.getElementById("order-id-input").value 
+console.log(orderId)
+document.getElementById('order-id-input').value=''
+// ChefReceived()
+// .then(PizzaSauceAdded)
+// .then(FirstLayerOfCheezeAdded)
+// .then(ToppingsAddedd)
+// .then(SecondLayerOfCheezeAdded)
+// .then(pizzaBaked)
+// .then(OreganoAddedAndPacked)
+// .then(PackageReceivedAtCounter)
+// .then(()=>console.log('The order is ready and handed over to the Zomato delivery guy!'))
+// .catch((errr)=>console.log(err))
+}
